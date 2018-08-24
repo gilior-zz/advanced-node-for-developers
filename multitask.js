af255aeee26edef9230ff1b92168d2d998656b94
lior gishry <liorgish@gmail.com>
2018-08-24 17:47:12 +0300

    Initial commit

---
+++ multitask.js
@@ -0,0 +1,39 @@
+const https = require('https')
+const crypto = require('crypto');
+const fs = require('fs');
+
+
+const start = Date.now();
+
+function doRequest() {
+    https.request('https://google.com', (res) => {
+        res.on('data', () => {
+        })
+        res.on('end', () => {
+            console.log('https.request', Date.now() - start)
+        });
+    })
+        .end();
+}
+
+
+function doHash() {
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+        console.log('hash: ', Date.now() - start)
+    })
+}
+
+doRequest();
+
+fs.readFile('multitask.js', 'utf8', () => {
+    console.log('fs.readFile', Date.now() - start)
+})
+
+doHash();
+doHash();
+doHash();
+doHash();
+
+
+
+
